7e981c4d51e522ede1884582ca78d03b0377657a
정의탁 <justicexkr23@gmail.com>
2025-05-16 20:30:52 +0900

    update

--- src/test/java/com/demo/variety_store_mono/VarietyStoreMonoApplicationTests.java
+++ src/test/java/com/demo/variety_store_mono/VarietyStoreMonoApplicationTests.java
@@ -2,8 +2,10 @@ package com.demo.variety_store_mono;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VarietyStoreMonoApplicationTests {
 
 	@Test
